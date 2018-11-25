45af4f3d016abc9226ed2a20b971d8ba54601dba
alodor <jose-a-mendez@hotmail.com>
2018-11-25 11:12:29 -0400

    Data cargada sobre los planetas

--- assets/js/function.js
+++ assets/js/function.js
@@ -2,15 +2,56 @@ new Vue({
     el: '#app',
     data: {
         peso: null,
-        see: false
+        see: false,
+        planets: [
+            {
+                name: 'Mercurio',
+                description: 'Es el planeta del sistema solar más cercano al Sol y el más pequeño.',
+                link: 'https://es.wikipedia.org/wiki/Mercurio_(planeta)'
+            },
+            {
+                name: 'Venus',
+                description: 'Es el segundo planeta del sistema solar cercano al Sol y el sexto más grande.',
+                link: 'https://es.wikipedia.org/wiki/Venus_(planeta)'
+            },
+            {
+                name: 'Tierra',
+                description: 'Es el tercer planeta del sistema solar cercano al Sol y el quinto más grande.',
+                link: 'https://es.wikipedia.org/wiki/Tierra'
+            },
+            {
+                name: 'Marte',
+                description: 'Es el cuarto planeta del sistema solar cercano al Sol y el segundo más pequeño.',
+                link: 'https://es.wikipedia.org/wiki/Marte_(planeta)'
+            },
+            {
+                name: 'Júpiter',
+                description: 'Es el quinto planeta del sistema solar cercano al Sol y el más grande.',
+                link: 'https://es.wikipedia.org/wiki/J%C3%BApiter_(planeta)'
+            },
+            {
+                name: 'Saturno',
+                description: 'Es el sexto planeta del sistema solar cercano al Sol y el segundo más grande.',
+                link: 'https://es.wikipedia.org/wiki/Saturno_(planeta)'
+            },
+            {
+                name: 'Urano',
+                description: 'Es el séptimo planeta del sistema solar cercano al Sol y el tercero más grande.',
+                link: 'https://es.wikipedia.org/wiki/Urano_(planeta)'
+            },
+            {
+                name: 'Neptuno',
+                description: 'Es el octavo planeta del sistema solar en distancia al sol y el más lejano.',
+                link: 'https://es.wikipedia.org/wiki/Neptuno_(planeta)'
+            },
+            {
+                name: 'Plutón',
+                description: 'Es un planeta enano del sistema solar situado a continuación de la órbita de Neptuno.',
+                link: 'https://es.wikipedia.org/wiki/Plut%C3%B3n_(planeta_enano)'
+            }
+        ]
     },
-    computed: {
-        calcularPesoTierra: function() {
-            let masa = parseInt(this.peso)
-            let peso = masa * 9.8
-            let conversion = peso * 0.102
-            return conversion.toFixed(2)
-        },
+    computed: {        
         calcularPesoMercurio: function() {
             let masa = parseInt(this.peso)
             let peso = masa * 3.70
@@ -20,6 +61,12 @@ new Vue({
         calcularPesoVenus: function() {
             let masa = parseInt(this.peso)
             let peso = masa * 8.87
+            let conversion = peso * 0.102
+            return conversion.toFixed(2)
+        },
+        calcularPesoTierra: function() {
+            let masa = parseInt(this.peso)
+            let peso = masa * 9.8
             let conversion = peso * 0.102
             return conversion.toFixed(2)
         },
